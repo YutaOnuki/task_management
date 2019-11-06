e3a04552543363c5da3f5a7d028a3bb79784c941
Yuta Onuki <onu4648yu@gmail.com>
2019-11-06 21:18:35 +0900

    feat:template MgtLoginView

--- frontend/src/components/templates/MgtLoginView.vue
+++ frontend/src/components/templates/MgtLoginView.vue
@@ -1,3 +1,34 @@
 <template>
-  <p>ログインページ</p>
+  <div class="login-view">
+    <h1>Task Management App</h1>
+    <MgtLoginForm :onlogin="handleLogin" />
+  </div>
 </template>
+
+<script>
+import MgtLoginForm from '@/components/molecules/MgtLoginForm.vue'
+
+export default {
+  name: 'MgtLoginView',
+  components: {
+    MgtLoginForm
+  },
+  methods: {
+    handleLogin (authInfo) {
+      return this.$store.dispatch('login', authInfo).then(() => {
+        this.$router.push({ path: '/' })
+      }).catch(err => this.throwReject(err))
+    },
+    throwReject (err) {
+      return Promise.reject(err)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .login-view {
+    width: 320px;
+    margin: auto;
+  }
+</style>
