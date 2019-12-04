cc8d37b128b5fe46dadb06ea3e632f2eafa33324
Yuta Onuki <onu4648yu@gmail.com>
2019-12-04 15:54:18 +0900

    wip:MgtTaskDetailModalスタイル作成途中

---
+++ frontend/src/components/molecules/MgtTaskDetailForm.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mgt-task-detail-contents">
+    <h4 class="mgt-task-detail-title">タスク詳細フォーム</h4>
+    <p class="mgt-task-detail-name">タスク名1</p>
+    <input class="mgt-task-detail-name-form"></input>
+  </div>
+</template>
