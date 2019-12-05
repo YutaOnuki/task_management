63f7a3334fd7d9be9f0c533b431bf419572e2198
Yuta Onuki <onu4648yu@gmail.com>
2019-12-05 15:57:30 +0900

    feat:MgtTaskDetail仮レイアウト

--- frontend/src/components/molecules/MgtTaskDetailForm.vue
+++ frontend/src/components/molecules/MgtTaskDetailForm.vue
@@ -2,6 +2,20 @@
   <div class="mgt-task-detail-contents">
     <h4 class="mgt-task-detail-title">タスク詳細フォーム</h4>
     <p class="mgt-task-detail-name">タスク名1</p>
-    <input class="mgt-task-detail-name-form"></input>
+    <input class="mgt-task-detail-name-form">
+    <p class="mgt-task-detail-description">説明</p>
+    <input class="mgt-task-detail-description-form">
+    <MgtButton>更新</MgtButton>
   </div>
 </template>
+
+<script>
+import MgtButton from '@/components/atoms/MgtButton'
+
+export default {
+  name: 'MgtTaskDetailForm',
+  components: {
+    MgtButton
+  }
+}
+</script>
