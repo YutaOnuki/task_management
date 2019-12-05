8d382f78cdb879af2358f5c6ec6921d6d2539c56
Yuta Onuki <onu4648yu@gmail.com>
2019-12-05 16:37:44 +0900

    chore:fontawesomeの導入

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,10 @@ import App from './App'
 import router from './router'
 import store from './store'
 import ErrorBoundary from './ErrorBoundary.vue'
+import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 
 Vue.config.productionTip = false
 
